159439860dcd405f21a10093343a31f0220bfa14
Saeideh Sabour <a.saboor1345@gmail.com>
2026-05-14 18:39:48 +0330

    Fixing the invoice part except "print", "download" and "shipping_method"

--- pcb_board/urls.py
+++ pcb_board/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("", PCBCreateView.as_view(), name="index_2"),
     path("orders/", OrdersListView.as_view(), name="orders_list"),
+    path("invoice/<int:pk>/", InvoiceDetailView.as_view(), name="invoice_detail"),
 ]
--- pcb_board/views.py
+++ pcb_board/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
 
 from .models import *
@@ -19,3 +19,17 @@ class OrdersListView(ListView):
     def get_queryset(self):
         queryset = Board.objects.filter(user = self.request.user.profile)
         return queryset
+    
+
+class InvoiceDetailView(DetailView):
+    model = Board
+    template_name = 'pcb_board/invoice_detail.html'
+    context_object_name = "data"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile"] = self.request.user.profile 
+        return context
+        
+
+    
